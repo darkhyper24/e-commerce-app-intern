9dbe25e43ee1c6121ad0c50b055e8c6b440fa711
darkhyper24 <s-mohammed.elmaadawy@zewailcity.edu.eg>
2025-07-21 18:07:31 +0300

    added database table

--- backend/src/init.sql
+++ backend/src/init.sql
@@ -1,11 +1,30 @@
 -- Enable UUID extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Create users table
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   username TEXT UNIQUE NOT NULL,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   phone TEXT
+);
+CREATE TABLE IF NOT EXISTS orders
+(
+    id uuid NOT NULL,
+    user_id uuid,
+    product_id uuid,
+    order_date date,
+    CONSTRAINT orders_pkey PRIMARY KEY (id),
+    CONSTRAINT product_id FOREIGN KEY (product_id) REFERENCES products (id) MATCH SIMPLE,
+    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users (id) MATCH SIMPLE
+);
+CREATE TABLE IF NOT EXISTS products
+(
+    id uuid NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    quantity integer NOT NULL,
+    description text COLLATE pg_catalog."default" NOT NULL,
+    category text COLLATE pg_catalog."default" NOT NULL,
+    photo bytea,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
 );
